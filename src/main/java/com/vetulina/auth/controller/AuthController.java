9b2721528f1de71be5b2006f0026d7b625d623c9
Asier Gonzalez <axigonzalez2012@gmail.com>
2024-04-25 18:48:09 +0200

    Añado nuevo controlador login2 para hacer pruebas

--- src/main/java/com/vetulina/auth/controller/AuthController.java
+++ src/main/java/com/vetulina/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.vetulina.auth.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,16 @@ public class AuthController {
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest loginRequest) {
         String token = authService.authenticate(loginRequest.getUsername(), loginRequest.getPassword());
+        if (token != null) {
+            return token;
+        } else {
+            return "Credenciales incorrectas";
+        }
+    }
+
+    @PostMapping("/login2")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        String token = authService.authenticate(username, password);
         if (token != null) {
             return token;
         } else {
